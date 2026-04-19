054c40b584d07e75bea1d95bfe9767340ed39b7e
전건욱 <jkw930221@gmail.com>
2026-04-19 17:50:33 +0900

    basic test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,73 @@
-#include <iostream>
+#include <windows.h>
 
-int main() {
-    std::cout << "반가워요" << std::endl;
-    return 0;
+namespace {
+
+constexpr wchar_t kWindowClassName[] = L"AbcWindowClass";
+constexpr wchar_t kWindowTitle[] = L"ABC App";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CREATE:
+        CreateWindowW(
+            L"STATIC",
+            L"abc",
+            WS_CHILD | WS_VISIBLE,
+            20,
+            20,
+            120,
+            30,
+            hwnd,
+            nullptr,
+            reinterpret_cast<LPCREATESTRUCTW>(lParam)->hInstance,
+            nullptr);
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
+}  // namespace
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+    WNDCLASSW window_class = {};
+    window_class.lpfnWndProc = WindowProc;
+    window_class.hInstance = hInstance;
+    window_class.lpszClassName = kWindowClassName;
+    window_class.hCursor = LoadCursorW(nullptr, reinterpret_cast<LPCWSTR>(IDC_ARROW));
+    window_class.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+
+    RegisterClassW(&window_class);
+
+    HWND hwnd = CreateWindowExW(
+        0,
+        kWindowClassName,
+        kWindowTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        320,
+        180,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr);
+
+    if (hwnd == nullptr) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = {};
+    while (GetMessageW(&msg, nullptr, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    return static_cast<int>(msg.wParam);
 }
